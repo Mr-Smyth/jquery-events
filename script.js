410ecded8782de66be62834b0de601f8589dc1cb
Eamonn Smyth <e.smyth75@gmail.com>
2020-05-29 16:09:04 +0000

    Added a mix of effects by chaining  in jQuery, on stream1 button

--- script.js
+++ script.js
@@ -2,36 +2,9 @@ $(document).ready(function() {
  
 
  	$("#stream1_btn").on('click',function() {
- 		$(".stream1").show();
+ 		$(".stream1").show().slideUp(2000).slideDown(2000).fadeTo(1000, 0.2);
         $(".stream2").show();
         $(".stream3").show();
-	  	$(".stream1").hide(10000);
-        $(".stream2").hide(10000);
-        $(".stream3").hide(10000);
-        $(".stream1").toggle(1000);
-        $(".stream2").toggle(1000);
-        $(".stream3").toggle(1000);
-        $(".stream1").slideDown(2000);
-        $(".stream2").slideDown(2000);
-        $(".stream3").slideDown(2000);
-        $(".stream1").show();
-        $(".stream2").show();
-        $(".stream3").show();
-        $(".stream1").slideUp(5000);
-        $(".stream2").slideUp(5000);
-        $(".stream3").slideUp(5000);
-        $(".stream1").slideToggle(1000);
-        $(".stream2").slideToggle(1000);
-        $(".stream3").slideToggle(1000);
-        $(".stream1").fadeOut(2000);
-        $(".stream2").fadeOut(2000);
-        $(".stream3").fadeOut(2000);
-        $(".stream1").fadeIn(4000);
-        $(".stream2").fadeIn(4000);
-        $(".stream3").fadeIn(4000);
-        $(".stream1").fadeTo(3000, 0.5);
-        $(".stream2").fadeTo(3000, 0.5);
-        $(".stream3").fadeTo(3000, 0.5);
 	});
 	/* $("#stream2_btn").click(function() {
 		$(".stream1").toggle(2000);
