7ddb36230b765d12bf7b1697d527de93bb997454
Eamonn Smyth <e.smyth75@gmail.com>
2020-05-29 11:26:50 +0000

    Added click and hide and toggle to test

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
  
 
- 	$("#stream1_btn").on("click", function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
+ 	$("#stream1_btn").click(function() {
+ 		$(".stream1").hide(2000);
+		$(".stream2").hide(2000);
+		$(".stream3").hide(2000);
+	  	
 	});
-	$("#stream2_btn").on("click", function() {
-		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream2").addClass('highlight_stream');
+	$("#stream2_btn").click(function() {
+		$(".stream1").toggle(2000);
+		$(".stream2").toggle(2000);
+		$(".stream3").toggle(2000);
+	  	
 	});
 	$("#stream3_btn").on("click", function() {
 		$(".stream1").removeClass('highlight_stream');
